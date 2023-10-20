8ad5c02ba1b20b52209bdc1c600704a66da8fc06
jbrennan414 <brennanj414@gmail.com>
2023-10-19 20:23:37 -0600

    refactor to functional component

--- lib/ski-bunks-stack.ts
+++ lib/ski-bunks-stack.ts
@@ -1,91 +1,125 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_dynamodb, aws_apigateway, aws_lambda } from 'aws-cdk-lib';
-import * as path from 'path';
 
 export class SkiBunkBackend extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+
+ constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create a DynamoDB table
     const dynamoTable = new aws_dynamodb.Table(this, 'SkiBunkDbTable', {
       partitionKey: { name: 'bed_id', type: aws_dynamodb.AttributeType.STRING },
       sortKey: { name: 'date', type: aws_dynamodb.AttributeType.STRING},
-      removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE, // Not recommended for production
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Not recommended for production
     });
 
     dynamoTable.addGlobalSecondaryIndex({
       indexName: 'date-index',
       partitionKey: { name: 'date', type: aws_dynamodb.AttributeType.STRING },
     });
-  
-    const createLambda = new aws_lambda.Function(this, 'CreateLambda', {
-      runtime: aws_lambda.Runtime.NODEJS_18_X,
-      handler: 'create.handler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
-      environment: {
-        DYNAMO_TABLE_NAME: dynamoTable.tableName,
-      },
-    });
 
     const readLambda = new aws_lambda.Function(this, "ReadLambda", {
       runtime: aws_lambda.Runtime.NODEJS_18_X,
       code: aws_lambda.Code.fromAsset("lib/lambda"),
       handler: "read.main",
-      environment: {
-        DYNAMO_TABLE_NAME: dynamoTable.tableName,
-      },
+        environment: {
+          DYNAMO_TABLE_NAME: dynamoTable.tableName,
+        },
     });
 
-    const updateLambda = new aws_lambda.Function(this, 'UpdateLambda', {
-      runtime: aws_lambda.Runtime.NODEJS_18_X,
-      handler: 'update.handler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
-      environment: {
-        DYNAMO_TABLE_NAME: dynamoTable.tableName,
-      },
-    });
+    dynamoTable.grantFullAccess(readLambda);
 
-    const deleteLambda = new aws_lambda.Function(this, 'DeleteLambda', {
-      runtime: aws_lambda.Runtime.NODEJS_18_X,
-      handler: 'delete.handler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
-      environment: {
-        DYNAMO_TABLE_NAME: dynamoTable.tableName,
-      },
-    });
-
-    dynamoTable.grantFullAccess(readLambda)
-
-    // Create an API Gateway and define CRUD methods
+    // Create an API Gateway
     const api = new aws_apigateway.RestApi(this, 'SkiBunksAPI');
 
-    const items = api.root.addResource('items');
-    items.addMethod('POST', new aws_apigateway.LambdaIntegration(createLambda));
-    items.addMethod('GET', new aws_apigateway.LambdaIntegration(readLambda), {
-      authorizationType: aws_apigateway.AuthorizationType.NONE,
-    });
-
-    const singleItem = items.addResource('{id}');
-    singleItem.addMethod('GET', new aws_apigateway.LambdaIntegration(readLambda));
-    singleItem.addMethod('PUT', new aws_apigateway.LambdaIntegration(updateLambda));
-    singleItem.addMethod('DELETE', new aws_apigateway.LambdaIntegration(deleteLambda));
-
-
-    // Deploy the API Gateway
-    const deployment = new aws_apigateway.Deployment(this, 'MyApiDeployment', {
-      api,
-    });
-    
-    new aws_apigateway.Stage(this, 'MyApiStage', {
-      deployment: deployment,
-      stageName: 'prod1',
-    });
-
-    // Output the URL of the API Gateway
-    new cdk.CfnOutput(this, 'MyApiUrl', {
-      value: api.url
-    });
-
+    // Define a resource and a GET method to access the Lambda function
+    api.root.addMethod('GET', new aws_apigateway.LambdaIntegration(readLambda));
   }
 }
+
+
+//   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     // Create a DynamoDB table
+//     const dynamoTable = new aws_dynamodb.Table(this, 'SkiBunkDbTable', {
+//       partitionKey: { name: 'bed_id', type: aws_dynamodb.AttributeType.STRING },
+//       sortKey: { name: 'date', type: aws_dynamodb.AttributeType.STRING},
+//       removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE, // Not recommended for production
+//     });
+
+//     dynamoTable.addGlobalSecondaryIndex({
+//       indexName: 'date-index',
+//       partitionKey: { name: 'date', type: aws_dynamodb.AttributeType.STRING },
+//     });
+  
+//     const createLambda = new aws_lambda.Function(this, 'CreateLambda', {
+//       runtime: aws_lambda.Runtime.NODEJS_18_X,
+//       handler: 'create.handler',
+//       code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+//       environment: {
+//         DYNAMO_TABLE_NAME: dynamoTable.tableName,
+//       },
+//     });
+
+//     const readLambda = new aws_lambda.Function(this, "ReadLambda", {
+//       runtime: aws_lambda.Runtime.NODEJS_18_X,
+//       code: aws_lambda.Code.fromAsset("lib/lambda"),
+//       handler: "read.main",
+//       environment: {
+//         DYNAMO_TABLE_NAME: dynamoTable.tableName,
+//       },
+//     });
+
+//     const updateLambda = new aws_lambda.Function(this, 'UpdateLambda', {
+//       runtime: aws_lambda.Runtime.NODEJS_18_X,
+//       handler: 'update.handler',
+//       code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+//       environment: {
+//         DYNAMO_TABLE_NAME: dynamoTable.tableName,
+//       },
+//     });
+
+//     const deleteLambda = new aws_lambda.Function(this, 'DeleteLambda', {
+//       runtime: aws_lambda.Runtime.NODEJS_18_X,
+//       handler: 'delete.handler',
+//       code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+//       environment: {
+//         DYNAMO_TABLE_NAME: dynamoTable.tableName,
+//       },
+//     });
+
+//     dynamoTable.grantFullAccess(readLambda)
+
+//     // Create an API Gateway and define CRUD methods
+//     const api = new aws_apigateway.RestApi(this, 'SkiBunksAPI');
+
+//     const items = api.root.addResource('items');
+//     items.addMethod('POST', new aws_apigateway.LambdaIntegration(createLambda));
+//     items.addMethod('GET', new aws_apigateway.LambdaIntegration(readLambda), {
+//       authorizationType: aws_apigateway.AuthorizationType.NONE,
+//       methodResponses: [{ statusCode: '200' }, { statusCode: '400' }, { statusCode: '500' }],
+      
+//     });
+
+//     const singleItem = items.addResource('{id}');
+//     singleItem.addMethod('GET', new aws_apigateway.LambdaIntegration(readLambda));
+//     singleItem.addMethod('PUT', new aws_apigateway.LambdaIntegration(updateLambda));
+//     singleItem.addMethod('DELETE', new aws_apigateway.LambdaIntegration(deleteLambda));
+
+
+//     // Deploy the API Gateway
+//     const deployment = new aws_apigateway.Deployment(this, 'MyApiDeployment', {
+//       api,
+//     });
+    
+//     new aws_apigateway.Stage(this, 'MyApiStage', {
+//       deployment: deployment,
+//       stageName: 'prod1',
+//     });
+
+
+
+//   }
+// }
 
--- ski-bunks-frontend/src/Calendar.js
+++ ski-bunks-frontend/src/Calendar.js
@@ -1,76 +1,65 @@
-import React from 'react';
+import React, {  useState } from 'react'
 import './Calendar.css';
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-class Calendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      month: props.month,
-    };
-  }
+export default function Calendar(props) {
 
-  getDaysInMonth(month, year) {
-    // Calculate the number of days in a month
+  const [month, setMonth] = useState(props.month);    
+
+  function getDaysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
   }
-
-  getStartingDay(month, year) {
+  
+  function getStartingDay(month, year) {
     // Calculate the starting day of the month (0 for Sunday, 1 for Monday, etc.)
     const startingDay = new Date(year, month - 1, 1).getDay();
     // Ensure the grid starts on Sunday
     return startingDay === 0 ? 7 : startingDay;
   }
-
-  renderDay(day) {
-    console.log(day);
+  
+  function renderDay(day) {
+    console.log(day); 
   }
+  
+  const year = new Date().getFullYear(); // You can change this to the desired year
 
-  render() {
+  const daysInMonth = getDaysInMonth(month, year);
+  const startingDay = getStartingDay(month, year);
 
-    const { month } = this.state;
-    const year = new Date().getFullYear(); // You can change this to the desired year
+  const monthArray = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+    'October', 'November', 'December'
+  ];
 
-    const daysInMonth = this.getDaysInMonth(month, year);
-    const startingDay = this.getStartingDay(month, year);
+  const readableMonth = monthArray[month - 1];
 
-    const monthArray = [
-      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
-      'October', 'November', 'December'
-    ];
+  const cells = [];
+  let day = 1;
 
-    const readableMonth = monthArray[month - 1];
-
-    const cells = [];
-    let day = 1;
-
-    // Create cells for each day in the calendar
-    for (let i = 0; i < 6; i++) {
-      for (let j = 0; j < 7; j++) {
-        if (i === 0 && j < startingDay) {
-          cells.push(<div key={`empty-${j}`} className="cell empty-cell"></div>);
-        } else if (day <= daysInMonth) {
-          cells.push(
-            <div id={day} key={day} className="cell" onClick={() => this.renderDay(day)}>
-              <Link to={`/day/${year}${month}${day}`}>{day}</Link>
-            </div>
-          );
-          day++;
-        }
+  // Create cells for each day in the calendar
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < startingDay) {
+        cells.push(<div key={`empty-${j}`} className="cell empty-cell"></div>);
+      } else if (day <= daysInMonth) {
+        cells.push(
+          <div id={day} key={day} className="cell" onClick={() => renderDay(day)}>
+            <Link to={`/day/${year}${month}${day}`}>{day}</Link>
+          </div>
+        );
+        day++;
       }
     }
-
-    return (
-      <div className="calendar">
-        <div>
-          <button onClick={() => this.setState({ month: month - 1 })}>Previous Month</button>
-          <h2>{readableMonth}</h2>
-          <button onClick={() => this.setState({ month: month + 1 })}>Next Month</button>
-        </div>
-        <div className="calendar-grid">{cells}</div>
-      </div>
-    );
   }
-}
 
-export default Calendar;
+  return (
+    <div className="calendar">
+      <div>
+        <button onClick={() => setMonth({ month: month - 1 })}>Previous Month</button>
+        <h2>{readableMonth}</h2>
+        <button onClick={() => setMonth({ month: month + 1 })}>Next Month</button>
+      </div>
+      <div className="calendar-grid">{cells}</div>
+    </div>
+  );
+}
