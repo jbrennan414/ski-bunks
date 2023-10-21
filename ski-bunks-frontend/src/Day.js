8a46b80b9e65507a3d0c390feb461343447baaa7
jbrennan414 <brennanj414@gmail.com>
2023-10-21 11:23:04 -0600

    wip

--- ski-bunks-frontend/src/Day.js
+++ ski-bunks-frontend/src/Day.js
@@ -1,7 +1,20 @@
-import React from 'react'
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import './Calendar.css';
+import axios from 'axios';
 
 export default function Day() {
+
+  // useEffect(() => {
+  //   axios.get(`https://fi9au6homh.execute-api.us-west-2.amazonaws.com/prod?month=${month}&year=${year}`)
+  //     .then((response) => {
+  //       setAvailableBeds(response.data);
+  //       setIsLoading(false);
+  //   }).catch((error) => {
+  //     console.log("ERRRRRRROR" , error);
+  //   });
+  // }, [month]);
+
   return (
     <div>
         <Link to="/">Back</Link>
