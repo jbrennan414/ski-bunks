3bb4b14e5eb804384638902fc9f0f7d4ee25dd61
jbrennan414 <brennanj414@gmail.com>
2023-10-21 23:29:40 -0600

    wip

--- ski-bunks-frontend/src/Bed.js
+++ ski-bunks-frontend/src/Bed.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
-import './Bed.css'
+import React from 'react'
 
 export default function Bed(props) {
 
   return (
     <div>
         <button onClick={() => props.setSelectedBed(props.bed_id)} disabled={ false }>
-            {<img src={props.occupantPhoto} alt='occupant-photo' className='photo'/>}{props.bed_id}
+            {props.isOccupied && "❌"}{props.bed_id}
         </button>
     </div>
   )
